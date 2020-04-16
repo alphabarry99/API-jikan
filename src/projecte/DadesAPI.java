656043a6267f9389999a5aecd20598c6601ff4e4
alphabarry99 <61462887+alphabarry99@users.noreply.github.com>
2020-04-16 17:14:38 +0200

    insercio de les classes

---
+++ src/projecte/DadesAPI.java
@@ -0,0 +1,12 @@
+
+package projecte;
+
+public class DadesAPI {
+
+    //substitueix ELEMENTS per un nom més adient per a les teves variables
+    static final int NUM_ELEMENTS1 = 100;
+    static final int NUM_ELEMENTS2 = 100;
+
+    String[] elements1 = new String[DadesAPI.NUM_ELEMENTS1];
+    String[] elements2 = new String[DadesAPI.NUM_ELEMENTS2];
+}
---
+++ src/projecte/M3_UF3_ActAvaluable.java
@@ -0,0 +1,28 @@
+package projecte;
+
+public class M3_UF3_ActAvaluable {
+
+
+    public static void main(String[] args) {
+        M3_UF3_ActAvaluable solucio = new M3_UF3_ActAvaluable();
+        solucio.inici();
+    }
+
+    /**
+     * Programa principal cridat des del main de la classe. Mostra el menú
+     * per pantalla i segons la opció triada executa una o altre instrucció fins
+     * que aquesta opció sigui la de sortir.
+     */
+    public void inici(){
+
+    }
+
+    /**
+     * Funció que s'encarrega de tota la gestió de les consultes.
+     * @param gestorDades variable que conté funcions de gestió de les dades de la API
+     * @param dadesAPI variable que conté les dades
+     */
+    public void consultar(GestorDades gestorDades, DadesAPI dadesAPI){
+
+    }
+}
