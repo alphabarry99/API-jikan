ddf7d80319ef0396575021d309445cebaba71e58
glopez1999 <66116631+glopez1999@users.noreply.github.com>
2020-05-29 12:09:53 +0200

    Update GestorDades.java
    Canvis en GestorDades

--- src/projecte/GestorDades.java
+++ src/projecte/GestorDades.java
@@ -1,31 +1,76 @@
 package projecte;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import project.uf4.model.ComplexEsportiu;
+import project.uf4.model.DadesAPI;
+
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 public class GestorDades {
+
+    private DadesAPI dades;
+
+    public GestorDades() {
+        dades = new DadesAPI();
+    }
     /**
-     * Carrega des de l'API les dades que ens interessin.
-     * @param dades parámetre que emmagatzemará les dades que hagim recuperat de l'API
+     * Carrega des de l'API les dades dels complexes
      */
-    public void carregarDades(DadesAPI dades){
+    public void carregarComplexes(){
+
+        try {
+            URL url = new URL("https://do.diba.cat/api/dataset/puntesports/pag-ini/1/pag-fi/100/format/json");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            JSONParser parser = new JSONParser();
+            JSONObject jsonObject = (JSONObject) parser.parse(new InputStreamReader(connection.getInputStream()));
+            JSONArray jsonArray = (JSONArray) jsonObject.get("elements");
+            for (int i = 0; i < jsonArray.size(); i++) {
+                JSONObject complexEsportiu = (JSONObject) jsonArray.get(i);
+                JSONObject dadesAdreca = (JSONObject) complexEsportiu.get("grup_adreca");
+                String nom = String.valueOf(complexEsportiu.get("adreca_nom"));
+                String municipi = String.valueOf(dadesAdreca.get("municipi_nom"));
+                ComplexEsportiu ce = new ComplexEsportiu(nom, municipi);
+                dades.afegirComplex(ce);
+            }
+            connection.disconnect();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void mostrarComplexes()  {
+
+        Interficie.mostrarMissatge(dades.getLlista());
     }
 
     /**
-     * Desa les dades de l'API en un fitxer txt extern
-     * @param dades conté les dades de l'API
+     * Desa les dades de l'API en un fitxer txt extern. Mètode de la UF3
      */
     public void desarDades(DadesAPI dades){
+
     }
     /**
-     * Ordena el llista d'elements alfabéticament independenment de si el nom es troba en majúscules o minúscules
-     * @param dades conté variable amb la informació dels alements a ordenar en l'interior
+     * Ordena el llista d'elements alfabéticament independenment de si el nom es troba en majúscules o minúscules. Mètode de la UF3
      */
-    private void ordenarElements(DadesAPI dades){
+    private void ordenarElements(){
     }
 
     /**
-     * Afegeix l'element a la llista. Es manté la ordenació alfabética
-     * @param dades conté variable amb la informació dels elements a canviar en l'interior
-     * @param nom nom de l'element que cal afegir
+     * Afegeix un nou element a la llista. Es manté la ordenació alfabética
      */
-    public void afegirElement(DadesAPI dades, String nom){
+    public void afegirElement(){
+        //Crear un nou complex i afegir-lo a la llista
     }
+
+    /**
+     * Esborra un element a la llista. Si no es troba, es notificarà a l'usuari
+     */
+    public void esborrarElement(){
+        //Es permetrà fer l'esborrat pel valor d'un atribut
+    }
+
 }
